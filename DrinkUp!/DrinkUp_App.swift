e689ec025d3934ecb2c35e98a6207291e96f24fd
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-05-18 12:49:04 +0200

    Initial Commit

---
+++ DrinkUp!/DrinkUp_App.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkUp_App.swift
+//  DrinkUp!
+//
+//  Created by Oliwier Kasprzak on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrinkUp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
